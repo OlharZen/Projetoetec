a85bce25320cdf3b8c1bc53261bb78e938636ad3
lab04 <lab04@lab4-08>
2024-09-02 20:42:20 -0300

    aula_01/09/2024

--- LojaETEC/src/br/com/etec/model/Operacoes.java
+++ LojaETEC/src/br/com/etec/model/Operacoes.java
@@ -2,6 +2,7 @@ package br.com.etec.model;
 
 import java.awt.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -15,24 +16,34 @@ public class Operacoes {
 	@FXML
 	private Button btnAcessar;
 	@FXML
-	private Button btnCadastrar;
+	private Button btnCadastrar; 
 	
 	@FXML
 	private void validarUsuario(ActionEvent event) {
 		String nomeUsuario;
 		nomeUsuario = txfUsuario.getText();
 		String senhaUsuario; 
-		nomeUsuario = psfSenha.getText();
+		senhaUsuario = psfSenha.getText();
 		
 		if(nomeUsuario.isEmpty() || senhaUsuario.isEmpty()) {
 			if(nomeUsuario.isEmpty()) {
+				mostrarAlerta(Alert.AlertType.WARNING, "Informe o nome !", " É necessário colocar o nome do usuário.");
 			//nome vazio
 		} else {
+			if(senhaUsuario.isEmpty()) {
+				mostrarAlerta(Alert.AlertType.WARNING, "Informe a senha !", " É necessário colocar a senha do usuário.");
 			//senha vazia
 		}
-		
-		
+		}
+			
 	}
 	
+	}
+	
+	private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensagem) {
+		Alert alerta = new Alert(tipo);
+		alerta.setTitle(titulo);
+		alerta.setContentText(mensagem);
+		alerta.showAndWait();
 	}
 }
